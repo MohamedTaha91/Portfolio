632534a742f88efb54bbbd73e7d4fdd96660b5fe
MohamedTaha91 <Taha_Acme@hotmial.com>
2024-03-20 21:59:22 +0100

    feat(searchbar): add searchbar component

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -3,34 +3,62 @@ import { ReactComponent as IconSearch } from 'theme/icons/search.svg';
 import styled from 'styled-components';
 
 const Container = styled.div`
-display: flex;
-border-radius: 25px;
-max-width: 250px;
-min-height:34px;
-border: 4px;
-color: #617798;
-border-style: double;
+  display: flex;
+  justify-content: center;
+  align-items:center;
+
+  max-width: 250px;
+  min-height:34px;
+
+  border: 1px solid #617798;
+  border-radius: 25px;
+
+  background-color: transparent;
+  color: #617798;
+  opacity: 1;
+
+  &:hover, &:focus-within {
+    background-color: #EAF0F6;
+    opacity: 0.45;
+  }
+  
+
+  &:focus-within {
+    outline: 1.5px solid #617798;
+    outline-offset: 2px;
+  }
 `
 
 const Input = styled.input`
-display:flex;
-border-radius:25px;
-align-items: stretch;
-min-width:215px;
-border: 2px;
-cursor: pointer;
-color:black;
+  display:flex;
+  border-radius:25px;
+  align-items: stretch;
+  min-width:215px;
+  cursor: pointer;
+  border: none;
+  outline:none;
+  color: #00234B; 
+  background-color: transparent;
+  &::placeholder {
+    color: #617798;
+  }
+  
 `
 
+const StyledIcon = styled(IconSearch)`
+  fill: #617798;
+  height: 16px;
+
+`
 
 
 export default function SearchBar() {
   // put the logic here
   return (
-  
+
     <Container>
-      <IconSearch/>
-      <Input type='text' placeholder='Search...'/> 
+      <StyledIcon />
+      <Input type='text' placeholder='Search...' />
     </Container>
   );
 }
